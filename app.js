4e2073ab93d100528d70eb0906a5abd62e3fd45b
Julian Garcia <69781917+julianalg@users.noreply.github.com>
2024-05-18 12:03:47 -0700

    switched to ejs

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const path = require('path');
+const bodyParser = require('body-parser')
+const ejs = require('ejs');
 
 const app = express();
 const port = 3000;
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(
     express.static(path.join(__dirname, "node_modules/bootstrap/dist/"))
 );
-app.use(express.static(__dirname, { // host the whole directory
-    extensions: ["html", "htm", "gif", "png"],
-}))
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.render('index.ejs')
 });
 
 app.listen(port, () => {
