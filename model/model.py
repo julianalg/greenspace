27a943640311c446cf7151e32f5e73eec2fdca76
Julian Garcia <69781917+julianalg@users.noreply.github.com>
2024-05-19 10:18:39 -0700

    preventing overfitting, model optimization

--- model/model.py
+++ model/model.py
@@ -5,12 +5,13 @@ import numpy as np
 import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 import joblib
 from sklearn.svm import SVC
 import warnings
-from flask import Flask, request, jsonify
+# from flask import Flask, request, jsonify
+from sklearn.tree import DecisionTreeClassifier
 warnings.filterwarnings("ignore")
 
 # Load the Iris dataset
@@ -22,8 +23,8 @@ y = data["Has Park"] # Labels
 # Split the dataset into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 # Initialize the SVM model
-model = SVC()
-# Train the model on the training dataa
+model = DecisionTreeClassifier()
+# Train the model on the training data
 model.fit(X_train, y_train)
 
 # Make predictions on the test data
@@ -41,40 +42,43 @@ class_report = classification_report(y_test, y_pred)
 # print('Classification Report:')
 # print(class_report)
 
-# FIXING OVERFITTING DATA 
+# FIXING OVERFITTING DATA, MEAN SQUARED ERROR AND MEAN ABSOLUTE ERROR
+def MSE(y,pred):
+    return np.mean((y-pred)**2)
+    
+def MAE(y,pred):
+    return np.mean(abs(y-pred))# load LA data
 
-# load LA data
+prediction = np.array([round(i) for i in model.predict(X_train)])
+MSE(y_train,prediction)
+MAE(y_train,prediction)
+
+prediction = np.array([round(i) for i in model.predict(X_test)])
+MSE(y_test,prediction)
+MAE(y_test,prediction)
+
+# FIXING OVERFITTING DATA, CROSS-VALIDATION
+from sklearn.model_selection import KFold, cross_val_score
+kfold = KFold(n_splits = 10)
+score_ten = cross_val_score(model,X,y, cv=kfold)
+print("10-Fold Cross Validation Scores are: {}".format(score_ten))
+print("Average 10-Fold Cross Validation score: {}".format(score_ten.mean()))
+
+rmse_score = abs(cross_val_score(model,X,y, scoring = 'neg_root_mean_squared_error', cv=kfold))
+print("RMSE Average 10-Fold Cross Validation Error: {}".format(rmse_score.mean()))
+
+# LOADING LA DATA
 data = pd.read_csv('model/data/la.csv').drop(["City"], axis=1)
 
 # Modify the land type column
 data['Land Type'] = data['Land Type'].replace({'Rural': 1, 'Suburban': 2, 'Urban': 3})
 
+# Regenerate prediction if percentage of 1 in y_pred array is more than 90%
 X = data
 y_pred = model.predict(X)
-print(f'Accuracy: {accuracy:.2f}')
+if np.mean(y_pred == 1) > 0.9:
+    y_pred = model.predict(X)
+else: 
+    print(y_pred)
 
-# joblib.dump(model, 'model.pkl')
-
-# # Python Web Server
-app = Flask(__name__)
-
-@app.route('/', methods=['POST'])
-def predict():
-    # Get the request data
-    data = request.json
-    
-    # Preprocess the data (if needed)
-    # ...
-    
-    # Make predictions using the trained model
-    predictions = model.predict(data)
-    
-    # Prepare the response
-    response = {
-        'predictions': predictions.tolist()
-    }
-    
-    return jsonify(response)
-
-if __name__ == '__main__':
-    app.run()
+joblib.dump(model, 'model.pkl')
